ce9b0428f5f7d8b879660043e18616c5ed1eae16
Ervin <ervinavdic1@hotmail.com>
2020-05-25 12:17:19 +0200

    Call response (Client)

--- Encoding2/Encoding2/Program.cs
+++ Encoding2/Encoding2/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
 
 namespace Encoding2
 {
@@ -6,7 +9,20 @@ namespace Encoding2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TcpClient client = new TcpClient();
+            int port = 5002;
+            IPAddress ip = IPAddress.Parse("172.16.113.179");
+            IPEndPoint endpoint = new IPEndPoint(ip, port);
+            client.Connect(endpoint);
+            NetworkStream stream = client.GetStream();
+
+
+            string text = "hello world!";
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+            stream.Write(buffer, 0, buffer.Length);
+
+            client.Close();
+
         }
     }
 }
