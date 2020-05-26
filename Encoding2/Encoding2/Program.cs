3e7c4079f642e1f2760dbaba1982ecc9b0a24d6e
Ervin <ervinavdic1@hotmail.com>
2020-05-26 14:36:26 +0200

    Den kan nu sende flere  beskeder af gangen, hvor den før kun kunne sende en

--- Encoding2/Encoding2/Program.cs
+++ Encoding2/Encoding2/Program.cs
@@ -1,32 +1,38 @@
 ﻿using System;
-using System.Net.Sockets;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
-
-namespace Encoding2
+namespace CallResponse
 {
-    class Program
+    class MainClass
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            int port = 5002;
+            Console.WriteLine("Hvilken port??");
+            int port = Convert.ToInt32(Console.ReadLine());
             bool running = true;
-
             menu();
-            while(running)
+            while (running)
             {
                 string input = Console.ReadLine();
-
                 Console.Clear();
                 menu();
-
                 if (input == "1")
                 {
-                    serverfunc(port);
+                    
+                        serverFunc(port);
+                    
+                   
                 }
                 else if (input == "2")
                 {
-                    clientfunc(port);
+                    Console.WriteLine("Skriv serverens ip adresse");
+                    string serverIP = Console.ReadLine();
+                    while (true)
+                    {
+                        clientFunc(port,serverIP);
+                    }
+                    
                 }
                 else if (input == "3")
                 {
@@ -34,98 +40,84 @@ namespace Encoding2
                 }
                 else
                 {
-                    Console.WriteLine("du skal skrive 1 eller 2");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Du skulle skrive 1 eller 2!!!!!!!!!");
+                    Console.ResetColor();
                 }
             }
-
-
-
-
-
         }
-
-        static void clientfunc(int port)
+        static void serverFunc(int port)
         {
-            TcpClient client = new TcpClient();
-            Console.WriteLine("Skriv serverens ip adresse");
-            string serverIP = Console.ReadLine();
-            IPAddress ip = IPAddress.Parse(serverIP);
-            IPEndPoint endpoint = new IPEndPoint(ip, port);
-            client.Connect(endpoint);
-            NetworkStream stream = client.GetStream();
-
-            Console.WriteLine("skriv din besked");
-            string text = Console.ReadLine(); ;
-            byte[] buffer = Encoding.UTF8.GetBytes(text);
-            stream.Write(buffer, 0, buffer.Length);
-
-
-            IPAddress ip1 = IPAddress.Any;
-            IPEndPoint endpoint1 = new IPEndPoint(ip1, port);
-
-            TcpListener listener = new TcpListener(endpoint1);
-            listener.Start();
-
-            TcpClient client1 = listener.AcceptTcpClient();
-
-            NetworkStream stream1 = client1.GetStream();
-
-            byte[] buffer1 = new byte[255];
-
-            int NumberOfBytes = stream1.Read(buffer1, 0, 255);
-
-            string converted = Encoding.UTF8.GetString(buffer1, 0, NumberOfBytes);
-
-            Console.WriteLine(converted);
-
-            listener.Stop();
-
+            while (true)
+            {
+                // Starts listener
+                TcpListener listener = StartListener(port);
+                Console.WriteLine("Awaiting Clients...");
+                // Gets message from client
+                getMessageFromStream(listener);
+                // Connects to client
+                TcpClient client = connect("172.16.113.179", port);
+                string text = "Det virkede!!!!!!!!!!";
+                //Send response message to client
+                sendMessage(text, client);
+            }
+            
         }
-
-        static void serverfunc(int port)
+        static void clientFunc(int port, string serverIP)
         {
-
-            IPAddress ip1 = IPAddress.Any;
-            IPEndPoint endpoint1 = new IPEndPoint(ip1, port);
-
-            TcpListener listener = new TcpListener(endpoint1);
-            listener.Start();
-
-            Console.WriteLine("Awaiting Clients....");
-
-            TcpClient client1 = listener.AcceptTcpClient();
-
-            NetworkStream stream1 = client1.GetStream();
-
-            byte[] buffer1 = new byte[255];
-
-            int NumberOfBytes = stream1.Read(buffer1, 0, 255);
-
-            string converted = Encoding.UTF8.GetString(buffer1, 0, NumberOfBytes);
-
-            Console.WriteLine(converted);
-
-            listener.Stop();
-
-            TcpClient client = new TcpClient();
-            Console.WriteLine("Skriv serverens ip adresse");
-            IPAddress ip = IPAddress.Parse("172.16.113.179");
-            IPEndPoint endpoint = new IPEndPoint(ip, port);
-            client.Connect(endpoint);
-            NetworkStream stream = client.GetStream();
-
-            byte[] buffer = Encoding.UTF8.GetBytes("Det virkede!!");
-            stream.Write(buffer, 0, buffer.Length);
-
-
-          
+            // Connects to server
+            TcpClient client = connect(serverIP, port);
+            Console.WriteLine("Skriv din besked");
+            string text = Console.ReadLine();
+            // Send message to server
+            sendMessage(text, client);
+            // Start listener
+            TcpListener listener = StartListener(port);
+            // Get message from server
+            getMessageFromStream(listener);
         }
         static void menu()
         {
-            Console.WriteLine("\n\n\nVil du være server eller client?");
+            Console.WriteLine("Vil du være server eller client");
             Console.WriteLine("Skriv 1 for at være server");
-            Console.WriteLine("skriv 2 for at være client");
-            Console.WriteLine("skriv 3 for at exit");
+            Console.WriteLine("Skriv 2 for at være client");
+            Console.WriteLine("Skriv 3 for at lukke programmet");
+        }
+        static TcpClient connect(string serverIP, int port)
+        {
+            TcpClient client = new TcpClient();
+            IPAddress ip = IPAddress.Parse(serverIP);
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+            client.Connect(endPoint);
+            return client;
+        }
+        static void sendMessage(string text, TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+            stream.Write(buffer, 0, buffer.Length);
+        }
+        static TcpListener StartListener(int port)
+        {
+            IPAddress ip1 = IPAddress.Any;
+            IPEndPoint endpoint1 = new IPEndPoint(ip1, port);
+            TcpListener listener = new TcpListener(endpoint1);
+            listener.Start();
+            return listener;
+        }
+        static void getMessageFromStream(TcpListener listener)
+        {
+            TcpClient client1 = listener.AcceptTcpClient();
+            NetworkStream stream1 = client1.GetStream();
+            byte[] buffer1 = new byte[255];
+            int numberOfBytes = stream1.Read(buffer1, 0, 255);
+            string converted = Encoding.UTF8.GetString(buffer1, 0,
+            numberOfBytes);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(converted);
+            Console.ResetColor();
+            listener.Stop();
         }
     }
 }
+
