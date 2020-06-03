512969ada066179b6e14f0669147cf7dd2a99eae
Ervin <ervinavdic1@hotmail.com>
2020-06-03 14:01:09 +0200

    Nye ændringer i udp.

---
+++ UDP/UDP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv 1 for at være Server");
+            Console.WriteLine("skriv 2 for at være client");
+
+            string input = Console.ReadLine();
+            if (input == "1")
+            {
+                new Server();
+            }
+            else if (input == "2")
+            {
+                new Client();
+            }
+        }
+    }
+}
