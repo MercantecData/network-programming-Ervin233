330511709fb0d063cd4ecbd0511b6cdcb86e5391
Ervin <ervinavdic1@hotmail.com>
2020-06-03 13:27:21 +0200

    Nu virker det med udp og den kan sende en besked og modtage efter hvad man vælger.

---
+++ Client.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+namespace UDP
+{
+    public class Client
+    {
+        public Client()
+        {
+            UdpClient client = new UdpClient();
+
+            string text = "Hello UDP!";
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("172.16.113.146"), 1234);
+
+            client.Send(bytes, bytes.Length, endpoint);
+        }
+    }
+}
